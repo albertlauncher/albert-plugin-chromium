5995456c61d8ead9b4d7f32a02a84886ada5c160
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:41 +0200

    feat: Add italian  translations

---
+++ i18n/chromium_it.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>BookmarkItem</name>
+    <message>
+        <source>Open URL</source>
+        <translation>Apri URL</translation>
+    </message>
+    <message>
+        <source>Copy URL to clipboard</source>
+        <translation>Copia URL negli appunti</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigWidget</name>
+    <message>
+        <source>Profile</source>
+        <translation>Profilo</translation>
+    </message>
+    <message>
+        <source>Match hostname</source>
+        <translation>Corrispondi nome host</translation>
+    </message>
+    <message>
+        <source>Show favicons</source>
+        <translation>Mostra favicon</translation>
+    </message>
+</context>
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>No profiles found.</source>
+        <translation>Nessun profilo trovato.</translation>
+    </message>
+</context>
+</TS>
